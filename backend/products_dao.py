4a0283861b308ae2425cc3e39a4e20993bbd50e2
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-02-14 07:22:57 +0530

    connection made

---
+++ backend/products_dao.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='root',
+                              host='127.0.0.1',
+                              database='grocery_store')
+cursor=cnx.cursor()
+
+query="Select * from products"
+cursor.execute(query)
+for (product_id,name,uom_id,price_per_unit) in cursor :
+    print (product_id,name,uom_id,price_per_unit)
+cnx.close()
