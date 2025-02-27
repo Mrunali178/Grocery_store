2983d2e04f4e6c4eb6cd00eb847f4371d5f96c00
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-02-27 19:34:11 +0530

    order dao and database connection

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ from flask import Flask, request,jsonify
 import products_dao
 from sql_connection import get_sql_connection
 import uom_dao
+import order_dao
 app = Flask (__name__)
 
 connection = get_sql_connection()
@@ -35,6 +36,24 @@ def delete_product():
     return_id=products_dao.delete_product(connection, request.form['product_id'])
     response = jsonify({'product_id': return_id})
     response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+@app.route('/insertOrder',methods=['POST'])
+def insert_order():
+    request_payload = json.loads(request.form['data'])
+
+    print("Received Payload:", request_payload)  
+
+    order_id=order_dao.insert_order(connection, request_payload)
+    response=jsonify({'order_id': order_id})
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+@app.route('/getAllOrders', methods=['GET'])
+def get_all_orders():
+    response = order_dao.get_all_orders(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 if __name__ == '__main__':
